9021f9292effd3299e73235cb8f9346c17fed825
Navau <milibrolunadepluton@gmail.com>
2022-11-29 16:03:05 -0400

    29/11/22

--- users/api/serializers.py
+++ users/api/serializers.py
@@ -6,5 +6,5 @@ from users.models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["id", "ci", "username", "email", "first_name",
+        fields = ["id", "username", "email", "first_name",
                   "last_name", "password", "is_active", "is_staff"]
